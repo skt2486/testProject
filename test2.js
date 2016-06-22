dbb9dca5ee264c0df817af315a8ab8f50ceb5abd
skt2486 <skt2486@naver.com>
2016-06-22 15:54:30 +0900

    -작업완료

--- test2.js
+++ test2.js
@@ -2,20 +2,7 @@ function test(a,b) {
     return a+b;
 }
 test(1,2)
-
-부장님이 시키신 작업 처리중
-완료
-sadfawe
-sadfawewaef
-awefawfe
-sadfwaef
-asdfawefasd
-zxcvsadf
-waerwer
-asdfawefasdsadf
-
-sadflkawejhfklj
-
-sadfkjjaskldfj
-
-완료
+dfdsfgdyrty
+ㅇㄶㄹㄴㄷㄳ
+ㄴㄷㄳㄴㄷㄱ
+ㅅㅈㄷㅅㅈㅅㄷ
